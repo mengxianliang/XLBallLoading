7402ac1d765e34c8a9dc9eef950b16ef6bf63c62
mengxianliang <meng_xian_liang@163.com>
2017-03-24 21:21:06 +0800

    更改了部分细节

--- XLBallLoadingDemo/XLBallLoading/XLBallLoading.m
+++ XLBallLoadingDemo/XLBallLoading/XLBallLoading.m
@@ -8,15 +8,15 @@
 
 #import "XLBallLoading.h"
 
-static CGFloat dotScale = 1.5f;
+static CGFloat ballScale = 1.5f;
 
 @interface XLBallLoading ()<CAAnimationDelegate>
 {
     
-    UIVisualEffectView *_dotContainer;
-    UIView *_dot1;
-    UIView *_dot2;
-    UIView *_dot3;
+    UIVisualEffectView *_ballContainer;
+    UIView *_ball1;
+    UIView *_ball2;
+    UIView *_ball3;
     
     BOOL _stopAnimationByUser;
 }
@@ -33,45 +33,45 @@ static CGFloat dotScale = 1.5f;
 
 -(void)initUI{
     
-    _dotContainer = [[UIVisualEffectView alloc]initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleLight]];
-    _dotContainer.frame = CGRectMake(0, 0, 100, 100);
-    _dotContainer.center = CGPointMake(self.bounds.size.width/2.0f, self.bounds.size.height/2.0f);
-    _dotContainer.layer.cornerRadius = 10.0f;
-    _dotContainer.layer.masksToBounds = true;
-    [self addSubview:_dotContainer];
+    _ballContainer = [[UIVisualEffectView alloc]initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleLight]];
+    _ballContainer.frame = CGRectMake(0, 0, 100, 100);
+    _ballContainer.center = CGPointMake(self.bounds.size.width/2.0f, self.bounds.size.height/2.0f);
+    _ballContainer.layer.cornerRadius = 10.0f;
+    _ballContainer.layer.masksToBounds = true;
+    [self addSubview:_ballContainer];
     
-    CGFloat dotWidth = 13.0f;
+    CGFloat ballWidth = 13.0f;
     
-    _dot1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, dotWidth, dotWidth)];
-    _dot1.center = CGPointMake(dotWidth/2.0f, _dotContainer.bounds.size.height/2.0f);
-    _dot1.layer.cornerRadius = dotWidth/2.0f;
-    _dot1.backgroundColor = [UIColor colorWithRed:54/255.0 green:136/255.0 blue:250/255.0 alpha:1];
-    [_dotContainer addSubview:_dot1];
+    _ball1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ballWidth, ballWidth)];
+    _ball1.center = CGPointMake(ballWidth/2.0f, _ballContainer.bounds.size.height/2.0f);
+    _ball1.layer.cornerRadius = ballWidth/2.0f;
+    _ball1.backgroundColor = [UIColor colorWithRed:54/255.0 green:136/255.0 blue:250/255.0 alpha:1];
+    [_ballContainer addSubview:_ball1];
     
-    _dot2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, dotWidth, dotWidth)];
-    _dot2.center = CGPointMake(_dotContainer.bounds.size.width/2.0f, _dotContainer.bounds.size.height/2.0f);
-    _dot2.layer.cornerRadius = dotWidth/2.0f;
-    _dot2.backgroundColor = [UIColor colorWithRed:100/255.0 green:100/255.0 blue:100/255.0 alpha:1];
-    [_dotContainer addSubview:_dot2];
+    _ball2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ballWidth, ballWidth)];
+    _ball2.center = CGPointMake(_ballContainer.bounds.size.width/2.0f, _ballContainer.bounds.size.height/2.0f);
+    _ball2.layer.cornerRadius = ballWidth/2.0f;
+    _ball2.backgroundColor = [UIColor colorWithRed:100/255.0 green:100/255.0 blue:100/255.0 alpha:1];
+    [_ballContainer addSubview:_ball2];
     
-    _dot3 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, dotWidth, dotWidth)];
-    _dot3.center = CGPointMake(_dotContainer.bounds.size.width - dotWidth/2.0f, _dotContainer.bounds.size.height/2.0f);
-    _dot3.layer.cornerRadius = dotWidth/2.0f;
-    _dot3.backgroundColor = [UIColor colorWithRed:234/255.0 green:67/255.0 blue:69/255.0 alpha:1];
-    [_dotContainer addSubview:_dot3];
+    _ball3 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ballWidth, ballWidth)];
+    _ball3.center = CGPointMake(_ballContainer.bounds.size.width - ballWidth/2.0f, _ballContainer.bounds.size.height/2.0f);
+    _ball3.layer.cornerRadius = ballWidth/2.0f;
+    _ball3.backgroundColor = [UIColor colorWithRed:234/255.0 green:67/255.0 blue:69/255.0 alpha:1];
+    [_ballContainer addSubview:_ball3];
 }
 
 -(void)startPathAnimate{
     
     //-------第一个球的动画
-    CGFloat width = _dotContainer.bounds.size.width;
+    CGFloat width = _ballContainer.bounds.size.width;
     //小圆半径
-    CGFloat r = (_dot1.bounds.size.width)*dotScale/2.0f;
+    CGFloat r = (_ball1.bounds.size.width)*ballScale/2.0f;
     //大圆半径
     CGFloat R = (width/2 + r)/2.0;
     
     UIBezierPath *path1 = [UIBezierPath bezierPath];
-    [path1 moveToPoint:_dot1.center];
+    [path1 moveToPoint:_ball1.center];
     //画大圆
     [path1 addArcWithCenter:CGPointMake(R + r, width/2) radius:R startAngle:M_PI endAngle:M_PI*2 clockwise:NO];
     //画小圆
@@ -79,19 +79,19 @@ static CGFloat dotScale = 1.5f;
     [path1_1 addArcWithCenter:CGPointMake(width/2, width/2) radius:r*2 startAngle:M_PI*2 endAngle:M_PI clockwise:NO];
     [path1 appendPath:path1_1];
     //回到原处
-    [path1 addLineToPoint:_dot1.center];
+    [path1 addLineToPoint:_ball1.center];
     //执行动画
     CAKeyframeAnimation *animation1 = [CAKeyframeAnimation animationWithKeyPath:@"position"];
     animation1.path = path1.CGPath;
     animation1.removedOnCompletion = YES;
     animation1.duration = [self animationDuration];
     animation1.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    [_dot1.layer addAnimation:animation1 forKey:@"animation1"];
+    [_ball1.layer addAnimation:animation1 forKey:@"animation1"];
     
     
     //-------第三个球的动画
     UIBezierPath *path3 = [UIBezierPath bezierPath];
-    [path3 moveToPoint:_dot3.center];
+    [path3 moveToPoint:_ball3.center];
     //画大圆
     [path3 addArcWithCenter:CGPointMake(width - (R + r), width/2) radius:R startAngle:2*M_PI endAngle:M_PI clockwise:NO];
     //画小圆
@@ -99,7 +99,7 @@ static CGFloat dotScale = 1.5f;
     [path3_1 addArcWithCenter:CGPointMake(width/2, width/2) radius:r*2 startAngle:M_PI endAngle:M_PI*2 clockwise:NO];
     [path3 appendPath:path3_1];
     //回到原处
-    [path3 addLineToPoint:_dot3.center];
+    [path3 addLineToPoint:_ball3.center];
     //执行动画
     CAKeyframeAnimation *animation3 = [CAKeyframeAnimation animationWithKeyPath:@"position"];
     animation3.path = path3.CGPath;
@@ -107,7 +107,7 @@ static CGFloat dotScale = 1.5f;
     animation3.duration = [self animationDuration];
     animation3.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
     animation3.delegate = self;
-    [_dot3.layer addAnimation:animation3 forKey:@"animation3"];
+    [_ball3.layer addAnimation:animation3 forKey:@"animation3"];
 }
 
 
@@ -118,14 +118,14 @@ static CGFloat dotScale = 1.5f;
     CGFloat duration = [self animationDuration]/2 - delay;
     
     [UIView animateWithDuration:duration delay:delay options:UIViewAnimationOptionCurveEaseOut| UIViewAnimationOptionBeginFromCurrentState animations:^{
-        _dot1.transform = CGAffineTransformMakeScale(dotScale, dotScale);
-        _dot2.transform = CGAffineTransformMakeScale(dotScale, dotScale);
-        _dot3.transform = CGAffineTransformMakeScale(dotScale, dotScale);
+        _ball1.transform = CGAffineTransformMakeScale(ballScale, ballScale);
+        _ball2.transform = CGAffineTransformMakeScale(ballScale, ballScale);
+        _ball3.transform = CGAffineTransformMakeScale(ballScale, ballScale);
     } completion:^(BOOL finished) {
         [UIView animateWithDuration:duration delay:delay options:UIViewAnimationOptionCurveEaseInOut| UIViewAnimationOptionBeginFromCurrentState animations:^{
-            _dot1.transform = CGAffineTransformIdentity;
-            _dot2.transform = CGAffineTransformIdentity;
-            _dot3.transform = CGAffineTransformIdentity;
+            _ball1.transform = CGAffineTransformIdentity;
+            _ball2.transform = CGAffineTransformIdentity;
+            _ball3.transform = CGAffineTransformIdentity;
         } completion:nil];
     }];
 }
@@ -149,12 +149,12 @@ static CGFloat dotScale = 1.5f;
 
 -(void)stop{
     _stopAnimationByUser = true;
-    [_dot1.layer removeAllAnimations];
-    [_dot1 removeFromSuperview];
-    [_dot2.layer removeAllAnimations];
-    [_dot2 removeFromSuperview];
-    [_dot3.layer removeAllAnimations];
-    [_dot3 removeFromSuperview];
+    [_ball1.layer removeAllAnimations];
+    [_ball1 removeFromSuperview];
+    [_ball2.layer removeAllAnimations];
+    [_ball2 removeFromSuperview];
+    [_ball3.layer removeAllAnimations];
+    [_ball3 removeFromSuperview];
 }
 
 +(void)showInView:(UIView *)view{
